5a9fb78752b5aa191ac0ee35049069198b0d8e55
Avishai Ish-Shalom <avishai@fewbytes.com>
2012-12-16 11:23:02 +0200

    Added missing source_type

--- recipes/java.rb
+++ recipes/java.rb
@@ -3,6 +3,7 @@ include_recipe "zeromq::dev"
 
 source_package "jzmq" do
   source "https://github.com/zeromq/jzmq.git"
+  source_type "git"
   ref "master"
   build_command "./autogen.sh && ./configure && make install"
   creates "/usr/local/share/java/zmq.jar"
