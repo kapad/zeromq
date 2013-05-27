e1a035b4b9ef9002f6f65bfdabebe7f41ffb7f12
Avishai Ish-Shalom <avishai@fewbytes.com>
2013-05-27 21:01:51 +0300

    remove java recipe

--- metadata.rb
+++ metadata.rb
@@ -4,8 +4,6 @@ maintainer_email "chef@fewbytes.com"
 license          "All rights reserved"
 description      "Installs/Configures zeromq"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.0"
+version          "0.2.0"
 
 depends          "git"
-depends          "source"
-depends          "java"
